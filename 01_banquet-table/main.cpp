38638211dde8b8779f35d0b09c6646ccc887b394
Evgenii Blinov <scythian89@gmail.com>
2025-08-29 21:06:56 +0300

    correction

--- 01_banquet-table/main.cpp
+++ 01_banquet-table/main.cpp
@@ -1,42 +1,19 @@
 #include <iostream>
-#include <vector>
-#include <string>
 
 
 int main() {
     std::cout << "===Banquet table===" << std::endl;
 
-    std::vector<std::vector<std::string> > cutlery = {
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife", "dessert spoon"},
-        {"fork", "spoon", "knife", "dessert spoon"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"},
-        {"fork", "spoon", "knife"}
-    };
+    int cutlery[2][6] = {{4, 3, 3, 3, 3, 3}, {4, 3, 3, 3, 3, 3}};
 
-    int dishes[12][1] = {
-        {2}, {2}, {2}, {2}, {2},
-        {3}, {3},
-        {2}, {2}, {2}, {2}, {2}
-    };
+    int dishes[2][6] = {{3, 2, 2, 2, 2, 2}, {3, 2, 2, 2, 2, 2}};
 
-    int chairs[12][1] = {
-        {1}, {1}, {1}, {1}, {1},
-        {1}, {1},
-        {1}, {1}, {1}, {1}, {1}
-    };
+    int chairs[2][6] = {{1, 1, 1, 1, 1, 1}, {1, 1, 1, 1, 1, 1}};
 
-    chairs[4][0] += 1;
-    cutlery[9][1] = "";
-    cutlery[6][1] = "";
-    cutlery[9][1] = "spoon";
-    dishes[6][0] -= 1;
+    chairs[0][4] += 1;
+    cutlery[1][2] -= 1;
+    cutlery[1][0] -= 1;
+    cutlery[1][2] += 1;
+    dishes[1][0] -= 1;
 
 }
