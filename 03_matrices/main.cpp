5e714f037b52376f2e4d9f7473d734149b8c6e44
Evgenii Blinov <scythian89@gmail.com>
2025-08-29 21:40:34 +0300

    correction

--- 03_matrices/main.cpp
+++ 03_matrices/main.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#include <vector>
 
-bool matrices_equal(std::vector<std::vector<int>> matrix_a, std::vector<std::vector<int>> matrix_b) {
 
-    if (matrix_a.size() != matrix_b.size()) {
-        return false;
-    }
+bool matrices_equal(int matrix_a[][4], int matrix_b[][4]) {
 
-    for (int i = 0; i < matrix_a.size(); i++) {
-        if (matrix_a[i].size() != matrix_b[i].size()) {
+    for (int i = 0; i < 4; i++) {
+        if (matrix_a[i] != matrix_b[i]) {
             return false;
         }
     }
 
-    for (int i = 0; i < matrix_a.size(); i++) {
-        for (int j = 0; j < matrix_a[i].size(); j++) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             if (matrix_a[i][j] != matrix_b[i][j]) {
                 return false;
             }
@@ -24,52 +20,47 @@ bool matrices_equal(std::vector<std::vector<int>> matrix_a, std::vector<std::vec
     return true;
 }
 
-std::vector<std::vector<int>> diagonal_matrix(std::vector<std::vector<int>> matrix_a) {
+void diagonal_matrix(int matrix_a[][4]) {
 
-    for (int i = 0; i < matrix_a.size(); i++) {
-        for (int j = 0; j < matrix_a[i].size(); j++) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
             if (i != j) {
                 matrix_a[i][j] = 0;
             }
         }
     }
-    return matrix_a;
 
 }
 
 int main() {
     std::cout << "===Matrices===" << std::endl;
 
-    std::vector<std::vector<int>> matrix_a;
-    std::vector<std::vector<int>> matrix_b;
+    int matrix_a[4][4];
+    int matrix_b[4][4];
     std::cout << "Enter matrix \'A\'" << std::endl;
 
     for (int i = 0; i < 4; i++) {
-        std::vector<int> row;
         for (int j = 0; j < 4; j++) {
             int element;
             std::cin >> element;
-            row.push_back(element);
+            matrix_a[i][j] = element;
         }
-        matrix_a.push_back(row);
     }
 
     std::cout << "Enter matrix \'B\'" << std::endl;
 
     for (int i = 0; i < 4; i++) {
-        std::vector<int> row;
         for (int j = 0; j < 4; j++) {
             int element;
             std::cin >> element;
-            row.push_back(element);
+            matrix_b[i][j] = element;
         }
-        matrix_b.push_back(row);
     }
 
     if (matrices_equal(matrix_a, matrix_b)) {
-        matrix_a = diagonal_matrix(matrix_a);
-        for (int i = 0; i < matrix_a.size(); i++) {
-            for (int j = 0; j < matrix_a[i].size(); j++) {
+        diagonal_matrix(matrix_a);
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
                 std::cout << matrix_a[i][j] << " ";
             }
             std::cout << std::endl;
